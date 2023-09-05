01ed2f94a7a9480962a0130f1e2a7529ce76faa8
Lalitha Madhurima Gayathri Kolluru <lkolluru@google.com>
2023-09-05 11:31:34 -0700

    changed object

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@
 #  * limitations under the License.
 #  */
 
-resource "google_storage_bucket_object" "bucket" {
+resource "google_storage_bucket" "bucket" {
   name                        = var.name
   project                     = var.project_id
   location                    = var.location
